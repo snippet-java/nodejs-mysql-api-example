fcc14f94edb0651007d4d2a84c642e171038a325
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-10-26 03:42:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+Node.js MySQL API Example
+====================================
+
+### MySQL (ClearDB) in BlueMix
+
+This repository is an example MySQL API enabled application that can be deployed into
+Bluemix with only a couple clicks. Try it out for yourself right now by clicking:
+
+[![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/snippet-java/nodejs-mysql-api-example.git)
+
+### How does this work?
+
+When you click the button, you are taken to Bluemix where you get a pick a name
+for your application at which point the platform takes over, grabs the code from
+this repository and gets it deployed.
+
+It will automatically create an instance of the ClearDB (MySQL) service, calling it
+`sample-cleardb` and bind it to your app. This is where your
+app will store its data. If you deploy multiple instances of
+app from this repository, they will share the same ClearDB instance.
+
+
+### Create, Read, Update, Delete (CRUD)
+
+This repository includes examples on the CRUD operations as APIs. It support both GET and POST requests.
+You should be able to create table, create row, list all rows, read single row, 
+update single row, and delete single row using the APIs.
+
+If you do clone this repository, make sure you update this `README.md` file to point
+the `Deploy to Bluemix` button at your repository.
+
+If you want to change the name of the ClearDB instance that gets created, the memory
+allocated to the application or other deploy-time options, have a look in `manifest.yml` and `.bluemix/pipeline.yml`.
+
+The method used in `.bluemix/pipeline.yml` will create an actual pipeline in DevOps.
+Where else the method used in the `manifest.yml` is a previous way to create a service. 
+
+### Privacy Notice
+
+This package is configured to track deployments to IBM Bluemix and other Cloud Foundry platforms. The following information is sent to a Deployment Tracker service on each deployment:
+
+* Node.js package version
+* Node.js repository URL
+* Application Name (application_name)
+* Space ID (space_id)
+* Application Version (application_version)
+* Application URIs (application_uris)
+* Labels of bound services
+* Number of instances for each bound service and associated plan information
+
+This data is collected from the package.json file in this application and the VCAP_APPLICATION and VCAP_SERVICES environment variables in IBM Bluemix and other Cloud Foundry platforms. This data is used by IBM to track metrics around deployments of this applications to IBM Bluemix to measure the usefulness of our examples, so that we can continuously improve the content we offer to you. Only deployments of sample applications that include code to ping the Deployment Tracker service will be tracked.
---
+++ app.js
@@ -0,0 +1,278 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql');
+
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+})); 
+
+var services = JSON.parse(process.env.VCAP_SERVICES) || {};
+
+var mysqlCreds = {};
+for (var serviceName in services) {
+	if (serviceName.indexOf("ClearDB") > -1) {
+		mysqlCreds = services[serviceName][0]['credentials'];
+	}
+}
+var config = require("./config.json");
+
+var connection = mysql.createConnection({
+	host     : mysqlCreds.host || config.mysql.host || "",
+	user     : mysqlCreds.user || config.mysql.user || "",
+	password : mysqlCreds.password || config.mysql.password || "",
+	database : mysqlCreds.database || config.mysql.database || ""
+});
+
+app.all(["/createtable"], function(req, res) {
+	// by default, will create a student table
+	
+	connection.connect();
+	
+	var query = "CREATE TABLE student ( \
+			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, \
+			firstname VARCHAR(30) NOT NULL, \
+			lastname VARCHAR(30) NOT NULL, \
+			student_id VARCHAR(50) \
+			)";
+	connection.query(query, function (err, results, fields) {
+		if (err) {
+			res.json({err:err});
+			return;
+		}
+		
+		res.json({results:results,fields:fields});
+	});
+	
+	connection.end();
+})
+
+app.get(["/create","/insert","/add"], function(req, res) {
+	//insert a default student row with the firstname John, lastname Doe & random student_id.
+	//accepts any key-value pair from POST & GET and assign to the doc as well.
+	
+	connection.connect();
+	
+	var values = req.body;
+	for (var key in req.query) {
+		values[key] = req.query[key]
+	}
+	var query = "INSERT INTO student SET ?";
+	connection.query(query, values, function (err, results, fields) {
+		if (err) {
+			res.json({err:err});
+			return;
+		}
+
+		res.json({results:results,fields:fields});
+	});
+	
+	connection.end();
+})
+
+app.all(["/list"], function(req, res) {
+	//list out all the rows in the student table
+	
+	connection.connect();
+	
+	var query = "SELECT * FROM student";
+	connection.query(query, function (err, results, fields) {
+		if (err) {
+			res.json({err:err});
+			return;
+		}
+
+		res.json({results:results,fields:fields});
+	});
+	
+	connection.end();
+})
+
+app.all(["/read","/get"], function(req, res) {
+	//read a row specified by the id in GET or POST.
+	//If none is specified, will read a random row from the table.
+	var id = req.query.id || req.body.id || "";
+	if (id != "") {
+		
+		connection.connect();
+		
+		var query = "SELECT * FROM student WHERE id=?";
+		connection.query(query, id, function (err, results, fields) {
+			if (err) {
+				res.json({err:err});
+				return;
+			}
+
+			res.json({results:results,fields:fields});
+		});
+		
+		connection.end();
+	} else {
+		getRandomRow(function(err, results, fields) {
+			if (err) {
+				res.json({err:err});
+				return;
+			}
+
+			res.json({results:results,fields:fields});
+		});
+	}
+})
+
+app.all(["/update","/modify"], function(req, res) {
+	//updates a row based on the id with a random student_id.
+	//accepts any key-value pair from POST & GET and apply to the row as well.
+	var id = req.query.id || req.body.id || "";
+	var values = {
+			student_id : generateStudentID()
+	}
+	for (var key in req.body) {
+		values[key] = req.body[key];
+	}
+	for (var key in req.query) {
+		values[key] = req.query[key];
+	}
+	if (id != "") {
+
+		connection.connect();
+
+		var query = "UDPATE student SET ? WHERE id=?";
+		connection.query(query, [values, id], function (err, results, fields) {
+			if (err) {
+				res.json({err:err});
+				return;
+			}
+
+			res.json({results:results,fields:fields});
+		});
+
+		connection.end();
+
+	} else {
+		getRandomRow(function(err, results, fields) {
+			if (err) {
+				res.json({err:err});
+				return;
+			}
+			
+			connection.connect();
+			
+			var id = results[0].id || "";
+			var query = "UDPATE student SET ? WHERE id=?";
+			connection.query(query, [values, id], function (err, results, fields) {
+				if (err) {
+					res.json({err:err});
+					return;
+				}
+
+				res.json({results:results,fields:fields});
+			});
+
+			connection.end();
+		});
+	}
+})
+
+
+app.all(["/delete","/destroy","/remove"], function(req, res) {
+	// deletes a row from the student table based on the id,
+	// otherwise, randomly delete 1 document
+	// accepts value from POST & GET
+	var id = req.query.id || req.body.id || "";
+
+	if (id != "") {
+		connection.connect();
+
+		var query = "DELETE FROM student id=?";
+		connection.query(query, id, function (err, results, fields) {
+			if (err) {
+				res.json({err:err});
+				return;
+			}
+
+			res.json({results:results,fields:fields});
+		});
+
+		connection.end();
+	} else {
+		getRandomRow(function(err, results, fields) {
+			if (err) {
+				res.json({err:err});
+				return;
+			}
+			
+			connection.connect();
+			
+			var id = results[0].id || "";
+			var query = "DELETE FROM student WHERE id=?";
+			connection.query(query, [values, id], function (err, results, fields) {
+				if (err) {
+					res.json({err:err});
+					return;
+				}
+
+				res.json({results:results,fields:fields});
+			});
+
+			connection.end();
+		});
+	}
+})
+
+app.all(["/deletedb","/destroydb","/removedb","/dropdb"], function(req, res) {
+	// deletes the student table,
+	// accepts table name value from POST & GET
+	var tablename = req.query.tablename || req.body.tablename || config.mysql.tableName || "";
+	
+	connection.connect();
+	
+	var query = "DROP ?";
+	connection.query(query, tablename, function (err, results, fields) {
+		if (err) {
+			res.json({err:err});
+			return;
+		}
+
+		res.json({results:results,fields:fields});
+	});
+
+	connection.end();
+})
+
+
+function generateStudentID() {
+	return "ID#" + Math.floor(Math.random()*10000);
+}
+
+// get a random row from the table if no specific id is selected
+function getRandomRow(cb) {
+	
+	connection.connect();
+	
+	var query = "SELECT * FROM student";
+	connection.query(query, function (err, results, fields) {
+		if (err) {
+			cb(err,null);
+			return;
+		} else if (results.length == 0) {
+			cb("Table is empty",null);
+			return;		
+		}
+
+		var row = results[Math.floor(Math.random()*results.length)];
+		cb(null,[row],fields);
+	});
+	
+	connection.end();
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
+require("cf-deployment-tracker-client").track();
