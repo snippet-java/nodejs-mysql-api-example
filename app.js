69d8ce7804d38d5d43f182901f4c38af5126eb9c
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-10-29 13:41:31 +0800

    updates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
+
 var mysql = require('mysql');
 
 app.set('port', process.env.PORT || 3000);
@@ -11,8 +12,8 @@ app.use(bodyParser.urlencoded({
 	extended: true
 })); 
 
+//==== CONFIGURE DATABASE ======================================//
 var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
-
 var mysqlCreds = {};
 for (var serviceName in services) {
 	if (serviceName.indexOf("cleardb") > -1) {
@@ -24,6 +25,7 @@ var services = {mysql:{}};
 if (JSON.stringify(mysqlCreds) == "{}") {
 	services = require("./services.json");
 }
+
 var dbConfig = {
 		host     : mysqlCreds.hostname || services.mysql.host || "",
 		user     : mysqlCreds.username || services.mysql.user || "",
@@ -35,41 +37,42 @@ var dbConfig = {
 var pool  = mysql.createPool(dbConfig);
 app.set('pool', pool);
 var db = require('./lib/mysql/db.js');
+//==============================================================//
 
 //==== CREATE STUDENT TABLE ====================================//
 // curl <url>/mysql/createtable                                 //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/createtable"], db.createTable);
+//==============================================================//
+app.all(db.paths.createTable, db.createTable);
 
 //==== CREATE / INSERT ROW =====================================//
 // curl <url>/mysql/create                                      //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/create","/mysql/insert","/mysql/add"], db.create);
+//==============================================================//
+app.all(db.paths.create, db.create);
 
 //==== LIST ALL ROWS IN TABLE===================================//
 // curl <url>/mysql/list                                        //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/list"], db.list);
+//==============================================================//
+app.all(db.paths.list, db.list);
 
 //==== READ 1 ROW BASED ON ID ==================================//
 // curl <url>/mysql/read?id=3                                   //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/read","/mysql/get"], db.read);
+//==============================================================//
+app.all(db.paths.read, db.read);
 
 //==== UPDATE 1 ROW BASED ON ID ================================//
 // curl <url>/mysql/update?id=3&firstname=Bob&lastname=Peter    //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/update","/mysql/modify"], db.update);
+//==============================================================//
+app.all(db.paths.update, db.update);
 
 //==== DELETE 1 ROW BASED ON ID ================================//
 //curl <url>/mysql/delete?id=3                                  //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/delete","/mysql/destroy","/mysql/remove"], db.deleteRow);
+//==============================================================//
+app.all(db.paths.delete_, db.delete_);
 
 //==== DELETE / DROP STUDENT TABLE =============================//
 //curl <url>/mysql/deletetable                                  //
-//////////////////////////////////////////////////////////////////
-app.all(["/mysql/deletetable","/mysql/droptable","/mysql/destroytable","/mysql/removetable"], db.deleteTable);
+//==============================================================//
+app.all(db.paths.deleteTable, db.deleteTable);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
